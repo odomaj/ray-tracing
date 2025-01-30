1e8f70c0703798cc3fd290d9e5c4d0a70e8978e4
odomaj <odomaj@clarkson.edu>
2025-01-29 21:58:40 -0500

    type adjustments

--- src/ray_tracing.py
+++ src/ray_tracing.py
@@ -23,7 +23,29 @@ class Sphere:
     shininess: np.float32
 
 
-def render_scene(pixels: Raster_RGB_NxM) -> Raster_RGB_NxM:
+@dataclass
+class Light:
+    position: Point_3D
+    intensity: np.float32
+    color: Color_RGB
+
+
+@dataclass
+class Scene:
+    image_height: np.int32
+    image_width: np.int32
+    image_plane_height: np.float32
+    image_plane_width: np.float32
+    aspect_ratio: np.float32
+    camera_position: Point_3D
+    image_plane_dist: np.float32
+
+
+def render_scene(
+    scene: Scene,
+    spheres: list[Sphere],
+    lights: list[Light],
+) -> Raster_RGB_NxM:
     """TODO:
     1. complete the function render_scene() to output the final image
     2. inside the render_scene() function, you need to implement:
@@ -31,62 +53,59 @@ def render_scene(pixels: Raster_RGB_NxM) -> Raster_RGB_NxM:
         ii)  Find the closest intersection
         iii) Shade each pixel using Blinn-Phong Shading
     """
+    pixels: Raster_RGB_NxM = np.zeros(
+        (scene.image_height, scene.image_width, 3), dtype=np.float32
+    )
     return pixels
 
 
-# Main function
 if __name__ == "__main__":
-    # Define spheres
     spheres: list[Sphere] = [
         # Cyan
         Sphere(
             center=np.array([0.0, 0.0, -5.0], dtype=np.float32),
-            radius=1.0,
+            radius=np.float32(1.0),
             color=np.array([0.0, 1.0, 1.0], dtype=np.float32),
-            ka=0.1,
-            kd=0.7,
-            ks=0.5,
-            shininess=32.0,
+            ka=np.float32(0.1),
+            kd=np.float32(0.7),
+            ks=np.float32(0.5),
+            shininess=np.float32(32.0),
         ),
         # Magenta
         Sphere(
             center=np.array([2.0, 0.0, -6.0], dtype=np.float32),
-            radius=1.5,
+            radius=np.float32(1.5),
             color=np.array([1.0, 0.0, 1.0], dtype=np.float32),
-            ka=0.1,
-            kd=0.7,
-            ks=0.5,
-            shininess=32.0,
+            ka=np.float32(0.1),
+            kd=np.float32(0.7),
+            ks=np.float32(0.5),
+            shininess=np.float32(32.0),
         ),
     ]
 
-    # Define light source
-    light_position: Point_3D = np.array([5, 5, -10], dtype=np.float32)
-    light_intensity: np.float32 = np.float32(1.0)
-    # White light
-    light_color = np.array([1.0, 1.0, 1.0], dtype=np.float32)
+    lights: list[Light] = [
+        Light(
+            position=np.array([5, 5, -10], dtype=np.float32),
+            intensity=np.float32(1.0),
+            color=np.ones(3, dtype=np.float32),
+        )
+    ]
 
-    # Render the scene
-    image_width: np.int32 = np.int32(800)
-    image_height: np.int32 = np.int32(800)
-
-    # Initial pixel colors of the scene (final output image)
-    pixel_colors: Raster_RGB_NxM = np.zeros(
-        (image_height, image_width, 3), dtype=np.float32
+    scene: Scene = Scene(
+        image_height=np.int32(800),
+        image_width=np.int32(800),
+        image_plane_height=np.float32(2.0),
+        image_plane_width=None,
+        aspect_ratio=None,
+        camera_position=np.zeros(3, dtype=np.float32),
+        image_plane_dist=np.float32(1.0),
     )
+    scene.aspect_ratio = np.float32(scene.image_width) / np.float32(
+        scene.image_height
+    )
+    scene.image_plane_width = scene.aspect_ratio * scene.image_plane_height
 
-    # Define the image plane
-    image_plane_height: np.float32 = np.float32(2.0)
-    aspect_ratio: np.float32 = image_width / image_height
-    image_plane_width: np.float32 = aspect_ratio * image_plane_height
-
-    # Define the camera
-    camera_position: Point_3D = np.array([0, 0, 0])
-
-    # Distance between the camera and the image plane
-    image_plane_dist: np.float32 = np.float32(1.0)
-
-    image: Raster_RGB_NxM = render_scene(pixel_colors)
+    image: Raster_RGB_NxM = render_scene(scene, spheres, lights)
 
     # Display the image
     plt.imshow(image)
